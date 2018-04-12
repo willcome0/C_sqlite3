f474668414ce4d347db864a14d28558593db5a37
willcome0 <willcome0@qq.com>
2018-04-12 09:39:03 +0800

    添加  创建表、清屏

--- project/main.cpp
+++ project/main.cpp
@@ -1,14 +1,29 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include "sqlite/sqlite3.h"
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+   int i;
+   for(i=0; i<argc; i++)
+   {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
 
-int main(void)
+int main(int argc, char* argv[])
 {
 	sqlite3 *db=NULL;
 	char *zErrMsg = 0;
 	int rc;
-	
-	rc = sqlite3_open("zieckey.db", &db); 
+	char *sql;			//表一
+
+	FILE *fp = fopen("debug.txt", "r");
+
+	/*连接数据库，不存在则创建*/
+	rc = sqlite3_open("shujuku.db", &db);
 	if( rc )
 	{
 		fprintf(stderr, "打开 %s 失败！\n", sqlite3_errmsg(db));
@@ -17,9 +32,33 @@ int main(void)
 	}
 	else
 		printf("打开成功！\n");
+	getchar();
+	system("CLS"); 
+	/**************************/
+
+	sql = "CREATE TABLE COMPANY("  \
+	     "ID INT PRIMARY KEY     NOT NULL," \
+	     "NAME           TEXT    NOT NULL," \
+	     "AGE            INT     NOT NULL," \
+	     "ADDRESS        CHAR(50)," \
+	     "SALARY         REAL );";
+
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	if( rc != SQLITE_OK )
+	{
+		printf("表创建失败 : %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else
+	{
+		printf("表创建成功\n");
+	}
+	getchar();
+	system("CLS"); 
 	
+	fclose(fp);
 	sqlite3_close(db); //关闭数据库
-	
+
 	return 0;
 
-}
+} 
